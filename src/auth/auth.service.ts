faa68a474bf793000ae63a5b90c14c37db3d3f9f
Arno2T <arnaud.tortora@gmail.com>
2026-07-16 00:48:08 +0200

    implement register & login routes with JWT

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,52 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { JwtService } from '@nestjs/jwt';
+import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+  ) {}
+
+  async register(dto: RegisterDto) {
+    const doesExist = await this.prisma.user.findUnique({
+      where: { email: dto.email },
+    });
+    if (doesExist) {
+      throw new Error('User already exist');
+    }
+
+    const hashedPassword = await bcrypt.hash(dto.password, 10);
+    await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        name: dto.name,
+        password: hashedPassword,
+      },
+    });
+    return { message: 'User created successfully' };
+  }
+
+  async login(dto: LoginDto) {
+    const user = await this.prisma.user.findUnique({
+      where: { email: dto.email },
+    });
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const isPasswordValid = await bcrypt.compare(dto.password, user.password);
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const payload = { sub: user.id, email: user.email };
+    return {
+      token: this.jwt.sign(payload),
+    };
+  }
+}
